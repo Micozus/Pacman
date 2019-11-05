d851ccba8bb3d5fcc7f623c85d7df7140878e5bd
Piotr <sixtyto@gmail.com>
2019-11-05 12:03:31 +0100

    Add map

--- main.js
+++ main.js
@@ -1,17 +1,79 @@
-const boardWidth = 27;
-const boardHeight = 29;
+const boardWidth = 28;
+const boardHeight = 31;
 const pacman = document.getElementById("pacman");
+const board = document.getElementById("board");
 
 const player = {
   id: pacman,
-  x: 0,
-  y: 0
+  x: 13,
+  y: 17
 };
+
+const boardMap = [
+  "############################",
+  "#............##............#",
+  "#.####.#####.##.#####.####.#",
+  "#.####.#####.##.#####.####.#",
+  "#.####.#####.##.#####.####.#",
+  "#..........................#",
+  "#.####.##.########.##.####.#",
+  "#.####.##.########.##.####.#",
+  "#......##....##............#",
+  "######.#####.##.#####.######",
+  "    ##.#####.##.#####.##    ",
+  "    ##.##..........##.##    ",
+  "    ##.##.########.##.##    ",
+  "######.##.########.##.######",
+  "..........########..........",
+  "######.##.########.##.######",
+  "    ##.##.########.##.##    ",
+  "    ##.##..........##.##    ",
+  "    ##.##.########.##.##    ",
+  "######.##.########.##.######",
+  "#............##............#",
+  "#.####.#####.##.#####.####.#",
+  "#.####.#####.##.#####.####.#",
+  "#...##................##...#",
+  "###.##.##.########.##.##.###",
+  "###.##.##.########.##.##.###",
+  "#......##....##....##......#",
+  "#.##########.##.##########.#",
+  "#.##########.##.##########.#",
+  "#..........................#",
+  "############################"
+];
+
+const allPositions = boardMap.flatMap((row, y) =>
+  row.split("").map((char, x) => ({ x, y, char }))
+);
+
+const drawWalls = (x, y) => {
+  const wall = document.createElement("div");
+  wall.classList.add("wall");
+  wall.style.gridArea = `${y + 1}/${x + 1}/${y + 2}/${x + 2}`;
+  board.appendChild(wall);
+};
+
+const drawDots = (x, y) => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dot.id = `dot${x}:${y}`;
+  dot.style.gridArea = `${y + 1}/${x + 1}/${y + 2}/${x + 2}`;
+  board.appendChild(dot);
+};
+
+const walls = allPositions.filter(item => item.char === "#");
+const dots = allPositions.filter(item => item.char === ".");
+
+walls.forEach(wall => drawWalls(wall.x, wall.y));
+dots.forEach(dot => drawDots(dot.x, dot.y));
 
 const updatePacman = () => {
   player.id.style.gridArea = `${player.y + 1}/${player.x + 1}/${player.y +
     2}/${player.x + 2}`;
 };
+
+updatePacman();
 
 window.addEventListener("keyup", event => {
   const currentPosition = {
@@ -40,5 +102,11 @@ window.addEventListener("keyup", event => {
   if (player.y < 0 || player.y > boardHeight - 1) {
     player.y = currentPosition.y;
   }
+  walls.forEach(wall => {
+    if (player.x === wall.x && player.y === wall.y) {
+      player.x = currentPosition.x;
+      player.y = currentPosition.y;
+    }
+  });
   updatePacman();
 });
